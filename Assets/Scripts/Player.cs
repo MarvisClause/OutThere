f1a7b9f74505460b863c91050ffbb1a3b2104ac2
Sturmik <2001bruzga2001@gmail.com>
2021-08-05 22:43:41 +0200

    Added player health and its lowering

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : BaseActiveObject
 {
     #region Variables
 
+    // Player max health 
+    [SerializeField] private int _playerHealthMaxCapacity;
+    // Player health for game session
+    private int _playerRecentHealth;
     // Player movement speed
     [SerializeField] private float _playerMovementSpeed;
     // Player rotation speed
@@ -14,10 +18,23 @@ public class Player : BaseActiveObject
     [SerializeField] private float _playerHitForce;
     // Player bullet
     [SerializeField] protected GameObject _playerProjectile;
+    // Is player hit
+    protected bool _isPlayerHit;
+    // Player hit cooldown time
+    [SerializeField] protected int _afterHitCooldownTime;
 
     #endregion
 
     #region Unity
+
+    // On player enable
+    protected void OnEnable()
+    {
+        // Setting player hit condition to false
+        _isPlayerHit = false;
+        // Setting player health back to maximum value
+        _playerRecentHealth = _playerHealthMaxCapacity;
+    }
 
     // Update is called once per frame
     protected override void Update()
@@ -47,15 +64,36 @@ public class Player : BaseActiveObject
         transform.Rotate(0.0f, 0.0f, -horizontalInput * _playerRotationSpeed * Time.deltaTime);
     }
 
+    // Object reaction to being hit
     protected override void Hit(Collision2D collision)
     {
+        // Player pushed away after hit
         _objectRigidbody.AddForce(_objectRigidbody.velocity * _playerHitForce * _playerHitForce, ForceMode2D.Force);
+        // Checking if player was hit or not
+        if (_isPlayerHit == false)
+        {
+            // Was player hit
+            _isPlayerHit = true;
+            // Decrease player health
+            _playerRecentHealth--;
+            // Invoke method
+            Invoke(nameof(HitCooldown), _afterHitCooldownTime);
+            // Logging damage
+            Debug.Log(_playerRecentHealth);
+        }
     }
      
+    // After player was hit there is time gap, where he can't be hit again
+    private void HitCooldown()
+    {
+        // Disabling player hit condition
+        _isPlayerHit = false;
+    }
+
     // Player fire
     private void Fire()
     {
-        Vector3 bulletPos = transform.position + transform.up ;
+        Vector3 bulletPos = transform.position + transform.up;
         Quaternion bulletRotate = transform.rotation;
        
        GameObject bulletInstance = SpawnManager.GetInstance().SpawnObject(SpawnManager.PoolType.PlayerBullets, _playerProjectile);
@@ -63,5 +101,6 @@ public class Player : BaseActiveObject
         bulletInstance.transform.position = bulletPos;
         bulletInstance.transform.rotation = bulletRotate;
     }
+
     #endregion
 }
