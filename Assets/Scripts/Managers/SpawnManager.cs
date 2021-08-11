c52537ff3bac15bf0b398602e7efd7ce0272023e
Sturmik <2001bruzga2001@gmail.com>
2021-08-11 12:31:59 +0200

    Reworked progression system in spawn manager

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -31,11 +31,21 @@ public class SpawnManager : MonoBehaviour
     }
     // Enemies to spawn
     [SerializeField] private List<GameObject> _enemiesToSpawn;
-    // Maximum amount of enemies on screen
-    [SerializeField] private int _maxEnemies;
+    // Initial amount of max enemies
+    [SerializeField] private int _initialMaxEnemies = 1;
+    // Recent max amount of enemies on screen
+    private int _recentMaxEnemies;
     // Time before spawn
     [SerializeField] private int _timeToRandomlySpawnFrom;
     [SerializeField] private int _timeToRandomlySpawnTo;
+    // Variable, which defines when new enemy will appear, depending on score value
+    [SerializeField] private int _newEnemyMultipleValue = 100;
+    // Last score value for new enemy multiply
+    private int _lastNewEnemyScore;
+    // Variable, which defines when new enemy will appear, depending on score value
+    [SerializeField] private int _newMaxQuantityOfEnemyMultipleValue = 50;
+    // Last score value for new max enemy quantity
+    private int _lastNewMaxEnemyQuantityScore;
     // Counter for active enemies on screen
     private int _activeEnemiesCounter;
     public int ActiveEnemiesCounter
@@ -43,6 +53,9 @@ public class SpawnManager : MonoBehaviour
         get { return _activeEnemiesCounter; }
         set { _activeEnemiesCounter = value; }
     }
+    // Variable for enemy spawn control, which depends on score
+    private int _maxSpawnIndex;
+
     // Pools
     private List<List<GameObject>> _pools;
 
@@ -71,6 +84,8 @@ public class SpawnManager : MonoBehaviour
     {
         // Checks, if we need to spawn new enemy
         SpawnEnemyCheck();
+        // Checks and react to player progression via score
+        ProgressionCheck();
     }
 
     #endregion
@@ -84,6 +99,10 @@ public class SpawnManager : MonoBehaviour
         CancelInvoke();
         // Setting active enemies counter to zero
         ActiveEnemiesCounter = 0;
+        // Setting max spawn index to zero
+        _maxSpawnIndex = 1;
+        // Resetting recent enemy quant value
+        _recentMaxEnemies = _initialMaxEnemies;
         // Iterate through each pool
         for (int poolIteration = 0; poolIteration < _pools.Count; poolIteration++)
         {
@@ -95,10 +114,46 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+    // Checks player progression and changes values according to it
+    private void ProgressionCheck()
+    {
+        if (GameManager.GetInstance().IsPlayerActive == true)
+        {
+            int recentScore = ScoreManager.GetInstance().PlayerScore;
+            // Progression system
+            if (recentScore != 0)
+            {
+                // When score reaches this condition, we add new type of enemy
+                if (recentScore % _newEnemyMultipleValue == 0
+                    && _maxSpawnIndex < _enemiesToSpawn.Count - 1
+                    && _lastNewEnemyScore != recentScore)
+                {
+                    _maxSpawnIndex++;
+                    _lastNewEnemyScore = recentScore;
+                    Debug.Log("Increase max spawn index to " + _maxSpawnIndex);
+                }
+                // When score reaches this condition, we increase max amount of enemies on screen
+                if (recentScore % _newMaxQuantityOfEnemyMultipleValue == 0
+                    && _lastNewMaxEnemyQuantityScore != recentScore)
+                {
+                    _recentMaxEnemies++;
+                    _lastNewMaxEnemyQuantityScore = recentScore;
+                    Debug.Log("Increase max amount of enemies to " + _recentMaxEnemies);
+                }
+            }
+        }
+    }
+
     // Spawns random enemy on screen
     private void SpawnEnemyCheck()
     {
-        if (ActiveEnemiesCounter < _maxEnemies)
+        if (GameManager.GetInstance().IsPlayerActive == false)
+        {
+            // This amount of enemies is used for main menu showcase.
+            // It is more for visual appearance, rather than gameplay
+            _recentMaxEnemies = 15;
+        }
+        if (ActiveEnemiesCounter < _recentMaxEnemies)
         {
             ActiveEnemiesCounter++;
             Invoke(nameof(SpawnEnemy), Random.Range(_timeToRandomlySpawnFrom, _timeToRandomlySpawnTo));
@@ -108,27 +163,7 @@ public class SpawnManager : MonoBehaviour
     // Spawns enemy
     private void SpawnEnemy()
     {
-        int spawnIndex = 0;
-        if (GameManager.GetInstance().IsPlayerActive == true)
-        {
-            // Getting random index of enemy depending on the score
-            int maxIndex = 0;
-            int recentScore = ScoreManager.GetInstance().PlayerScore;
-            if (recentScore > 50 && recentScore < 150)
-            {
-                maxIndex = 2; 
-            }
-            else
-                if (recentScore > 150)
-            {
-                maxIndex = 3;
-            }
-            spawnIndex = Random.Range(0, maxIndex);
-        }
-        if (spawnIndex >= _enemiesToSpawn.Count)
-        {
-            spawnIndex = _enemiesToSpawn.Count - 1;
-        }
+        int spawnIndex = Random.Range(0, _maxSpawnIndex);
         SpawnObject((PoolType)spawnIndex, _enemiesToSpawn[spawnIndex]);
     }
 
